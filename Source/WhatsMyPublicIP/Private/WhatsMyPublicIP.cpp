f74293e59371a2931491e20541f668bd31de5353
Alex Hajdu <alexhajdu@me.com>
2018-05-31 21:38:45 +0200

    Initial commit

---
+++ Source/WhatsMyPublicIP/Private/WhatsMyPublicIP.cpp
@@ -0,0 +1,82 @@
+// Alex Hajdu, (C) 2018, alexhajdu[at]me.com, twitter.com/alexhajdu
+
+#include "WhatsMyPublicIP.h"
+#include <JsonObject.h>
+#include <JsonReader.h>
+#include <JsonSerializer.h>
+
+UWhatsMyPublicIP::UWhatsMyPublicIP( )
+{
+	IP		= TEXT( "" );
+	APIUrl  = TEXT( "https://api.ipify.org/?format=json" );
+	JSONKey = TEXT( "api" );
+}
+
+UWhatsMyPublicIP::~UWhatsMyPublicIP( )
+{
+}
+
+void UWhatsMyPublicIP::GetMyPublicIP( )
+{
+	IP   = TEXT( "" );
+	Http = &FHttpModule::Get( );
+
+	if( !Http )
+	{
+		return;
+	}
+
+	DoRequest( );
+}
+
+FString UWhatsMyPublicIP::GetCachedIP( ) const
+{
+	if( IP.IsEmpty( ) )
+	{
+		UE_LOG( LogTemp, Warning, TEXT( "Get My Public IP : Your public IP addres not obtained yet. Processing..." ) );
+	}
+
+	return IP;
+}
+
+void UWhatsMyPublicIP::DoRequest( )
+{
+	TSharedRef< IHttpRequest > Request = Http->CreateRequest( );
+	Request->OnProcessRequestComplete( ).BindUObject( this, &UWhatsMyPublicIP::OnResponseReceived );
+	Request->SetURL( APIUrl );
+	Request->SetVerb( "GET" );
+	Request->SetHeader( TEXT( "User-Agent" ), "X-UnrealEngine-Agent" );
+	Request->SetHeader( "Content-Type", TEXT( "application/json" ) );
+	Request->ProcessRequest( );
+}
+
+void UWhatsMyPublicIP::OnResponseReceived( FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful )
+{
+	if (bWasSuccessful)
+	{
+
+		TSharedPtr< FJsonObject > JsonObject;
+
+		TSharedRef< TJsonReader<> > Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			IP = JsonObject->GetStringField("ip");
+			OnIPAddressReceived.Broadcast(GetCachedIP());
+		}
+	}
+	else
+	{
+		UE_LOG( LogTemp, Warning, TEXT( "Get My Public IP : Request failed." ) );
+	}
+}
+
+void UWhatsMyPublicIP::OverrideJSONKey( FString NewJsonKey )
+{
+	JSONKey = NewJsonKey;
+}
+
+void UWhatsMyPublicIP::OverrideAPIUrl( FString NewAPIUrl )
+{
+	APIUrl = NewAPIUrl;
+}
---
+++ Source/WhatsMyPublicIP/Public/WhatsMyPublicIP.h
@@ -0,0 +1,60 @@
+// Alex Hajdu, (C) 2018, alexhajdu[at]me.com, twitter.com/alexhajdu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include <UnrealString.h>
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "WhatsMyPublicIP.generated.h"
+
+class FHttpModule;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FIPDelegate, FString, IP );
+
+UCLASS( BlueprintType, Blueprintable )
+class WHATSMYPUBLICIP_API UWhatsMyPublicIP : public UObject
+{
+	GENERATED_BODY( )
+
+	/**
+	 * Lifetime
+	 */
+
+public:
+	UWhatsMyPublicIP( );
+	~UWhatsMyPublicIP( );
+
+	/**
+	 * API
+	 */
+
+public:
+	/** Main function to call in order to get your public IP */
+	UFUNCTION( BlueprintCallable, Category = "Networking" )
+	void GetMyPublicIP( );
+
+	/** Getter for IP string. Returns empty string if still no response from server. */
+	UFUNCTION( BlueprintCallable, Category = "Networking" )
+	FString GetCachedIP( ) const;
+
+	/**Set your own API url if you want. */
+	UFUNCTION( BlueprintCallable, Category = "Networking" )
+	void OverrideAPIUrl( FString NewAPIUrl );
+
+	/**Set necessary key to get value from json to fit your API response. Note: works only on simple object. */
+	UFUNCTION( BlueprintCallable, Category = "Networking" )
+	void OverrideJSONKey( FString NewJsonKey );
+
+	/** Subscribe to this event and get your IP address safely */
+	UPROPERTY( BlueprintAssignable, Category = "Networking" )
+	FIPDelegate OnIPAddressReceived;
+
+private:
+	FHttpModule* Http;
+	FString		 IP;
+	FString		 APIUrl;
+	FString		 JSONKey;
+	void		 DoRequest( );
+	void		 OnResponseReceived( FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful );
+};
